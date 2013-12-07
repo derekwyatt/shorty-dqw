c5323ed3842e2f7d0c5fca133edfb28437cb4ab3
Derek Wyatt <derek@derekwyatt.org>
2013-12-07 16:27:33 -0500

    Probably need to set to port 80

--- src/main/scala/org/derekwyatt/shorty/Boot.scala
+++ src/main/scala/org/derekwyatt/shorty/Boot.scala
@@ -7,5 +7,5 @@ import spray.can.Http
 object Boot extends App {
   implicit val system = ActorSystem("Shorty")
   val service = system.actorOf(ShortyServiceListener.props(), "ShortyServiceListener")
-  IO(Http) ! Http.Bind(service, "0.0.0.0", 8080)
+  IO(Http) ! Http.Bind(service, "0.0.0.0", 80)
 }
