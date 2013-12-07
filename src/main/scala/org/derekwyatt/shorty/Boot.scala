de58138e888079ecd6f2bdbd11f768287fdb11c2
Derek Wyatt <derek@derekwyatt.org>
2013-12-07 17:02:11 -0500

    Set up the port for usage on Heroku

--- src/main/scala/org/derekwyatt/shorty/Boot.scala
+++ src/main/scala/org/derekwyatt/shorty/Boot.scala
@@ -2,10 +2,12 @@ package org.derekwyatt.shorty
 
 import akka.actor.ActorSystem
 import akka.io.IO
+import scala.util.Properties
 import spray.can.Http
 
 object Boot extends App {
   implicit val system = ActorSystem("Shorty")
   val service = system.actorOf(ShortyServiceListener.props(), "ShortyServiceListener")
-  IO(Http) ! Http.Bind(service, "0.0.0.0", 80)
+  val port = Properties.envOrElse("PORT", "8080").toInt
+  IO(Http) ! Http.Bind(service, "0.0.0.0", port)
 }
